7c57ea09cc1fd1f650ecdb2cf1d7c0fef77c8482
Suborna Nath <subornadnath@gmail.com>
2022-10-15 20:51:27 -0400

    Update test.cy.js

--- cypress_test/cypress/e2e/test.cy.js
+++ cypress_test/cypress/e2e/test.cy.js
@@ -1,6 +1,6 @@
 describe('Heading', () => {
     it('has the right title', () => {
-        cy.visit('http://url-shortner-dev.us-east-1.elasticbeanstalk.com/')
+        cy.visit('http://54.144.111.183:5000')
 
         cy.get('title')
             .invoke('text')
