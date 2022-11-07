aa74aeaf45f4f796ab2ccc731d1d9feaa690e84c
Suborna Nath <subornadnath@gmail.com>
2022-11-06 22:09:49 -0500

    Update cypress.config.js

--- cypress_test/cypress.config.js
+++ cypress_test/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://54.144.111.183:5000',
+    baseUrl: 'http://127.0.0.1:8000',
     reporter: 'junit',
     reporterOptions: {
       mochaFile: 'results/cypress-this.reporter.xml',
